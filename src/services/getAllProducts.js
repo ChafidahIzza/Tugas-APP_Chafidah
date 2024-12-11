a6f8e479cc857b252d2a5da141233bbd18477bca
ChafidahIzza <chafidahizza17@gmail.com>
2024-12-11 16:27:53 +0700

    first init github

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,34 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: "Stray Kids- S Class-Keychain-Classy Ver-001",
+      slug: "Sray Kids-S Class-Keychain-Classy Ver-Keychain",
+      name: "Stray Kids S-Class Keychain Classy Ver",
+      category: "Keychain",
+      price: 39_000,
+      stock: 20,
+      imageUrl: "/assets/images/WhatsApp Image 2024-10-26 at 21.11.17_aa3a7833.jpg",
+      description: `Acrylic Double Side, Thickness 3 mm, Main charm size 6 cm, Mini charm, Star charm`,
+    },
+    {
+      id: "Stray Kids- S Class-Keychain-Lovely Ver-001",
+      slug: "Stray Kids- S Class-Keychain-Lovely Ver-Keychain",
+      name: "Stray Kids S Class-Keychain Lovely Ver",
+      category: "Keychain",
+      price: 37_000,
+      stock: 5,
+      imageUrl: "/assets/images/WhatsApp Image 2024-10-26 at 21.11.15_f533fb9d.jpg",
+      description: `Acrylic Double Side, Thickness 3 mm, Main charm size 6 cm, Mini charm, Star charm`,
+    },
+    {
+      id: "Stray Kids-S Class-Sticker Pack-001",
+      slug: "Stray Kids-S Class-Sticker Pack-Sticker",
+      name: "Stray Kids S Class-Sticker Pack",
+      category: "Sticker Pack",
+      price: 15_000,
+      stock: 0,
+      imageUrl: "/assets/images/STK.jpg",
+      description: `A6 Vinyl Paper, Glitter Lamination, Kisscut`,
+    },
+  ];
+}
